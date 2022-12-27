4a3ea1a906b5caf9924e7cceb72b049c42a04474
Akash Koodali <akashkoodali9@gmail.com>
2022-12-28 01:11:34 +0530

    created endpoints for connect, disconnect, getClients in handlers.ts file.

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,6 +1,25 @@
 import { APIGatewayProxyEvent, APIGatewayProxyEventQueryStringParameters, APIGatewayProxyResult } from "aws-lambda";
+import AWS, { AWSError } from 'aws-sdk';
 
 type Action = "$connect" | "$disconnect" | "getMessages" | "sendMessage" | "getClients";
+
+type Client = {
+  connectionId :string,
+  nickname: string
+}
+
+const CLIENT_TABLE_NAME = "Clients";
+
+const responseOk = {
+  statusCode: 200,
+  body: ""
+}
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const apiGw = new AWS.ApiGatewayManagementApi({
+  endpoint: process.env["WSSAPIGATEWAYENDPOINT"]
+});
 
 export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   //
@@ -10,6 +29,11 @@ export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayPro
   switch (routeKey) {
     case "$connect":
       return handleConnect(connectionId, event.queryStringParameters);
+    case "$disconnect":
+      return handleDisconnect(connectionId);
+    case "getClients":
+      return handleGetClients(connectionId);
+
     default:
       return {
         statusCode: 403,
@@ -29,4 +53,88 @@ const handleConnect = async (
       body: ""
     }
   }
+
+  await docClient.put({
+    TableName: CLIENT_TABLE_NAME,
+    Item: {
+      connectionId,
+      nickname: queryParams["nickname"]
+    }
+  }).promise();
+
+  await notifyClients(connectionId);
+
+  return responseOk;
 };
+
+
+
+const handleDisconnect = async (connectionId: string): Promise<APIGatewayProxyResult> => {
+
+  await docClient.delete({
+    TableName: CLIENT_TABLE_NAME,
+   Key: {
+    connectionId,
+   }
+  }).promise();
+
+  await notifyClients(connectionId);
+
+  return responseOk;
+};
+
+
+const notifyClients = async (connectionIdToExclude: string) => {
+  const clients = await getAllClients();
+
+await Promise.all(clients.filter((client) => 
+  client.connectionId !== connectionIdToExclude)
+  .map(async (client) => {
+    await postToConnection(client.connectionId, JSON.stringify(clients));
+  }));
+};
+
+const getAllClients = async (): Promise<Client[]> => {
+  const output = await docClient.scan({
+    TableName: CLIENT_TABLE_NAME
+  }).promise();
+
+  const clients =  output.Items || [];
+
+  return clients as Client[]
+}
+
+
+const handleGetClients = async (connectionId: string): Promise<APIGatewayProxyResult> => {
+
+  const clients = await getAllClients();
+
+  await postToConnection(connectionId, JSON.stringify(clients));
+  
+  return responseOk;
+}
+
+
+
+const postToConnection =async (connectionId :string, data: string) => {
+  
+  try {
+    await apiGw.postToConnection({
+      ConnectionId: connectionId,
+      Data: data,  //JSON.stringify(clients),
+    }).promise();
+ 
+  } catch (error) {
+    if((error as AWSError).statusCode !== 410) {
+      throw error;
+    }
+      await docClient.delete({
+        TableName: CLIENT_TABLE_NAME,
+       Key: {
+        connectionId,
+       }
+      })
+      .promise(); 
+  }
+
+}
